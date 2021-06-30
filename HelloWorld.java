f9369145ad23e93bbf7471eb4a8f5b32dc370f1c
MaraAndersone <86737998+MaraAndersone@users.noreply.github.com>
2021-06-30 21:16:28 +0300

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,5 +3,6 @@ public class HelloWorld {
     public static void main(String[] args) {
 	// write your code here
         System.out.println("Hello World!");
+	          System.out.println("Maara");
     }
 }
