af3023dadf3408c7d19672481fb92d09394a30e8
SantaKolosovska <86738044+SantaKolosovska@users.noreply.github.com>
2021-07-07 21:26:37 +0300

    Update Main.java
    the results

--- Project3/src/Main.java
+++ Project3/src/Main.java
@@ -4,8 +4,20 @@ public class Main {
 //        Write a Java program calculates and prints the result of
 //   the following operations.
 //      a. -5 + 8 * 6
+
+        int result1 = (-5 + 8 * 6);
+        System.out.println(result1);
+
 //      b. (55+9) % 9
+
+        int result2 = ((55 + 9) % 9);
+        System.out.println(result2);
+
 //      c. 5 + 15 / 3 * 2 - 8 % 3
+
+        int result3 = (5 + 15 / 3 * 2 - 8 % 3);
+        System.out.println(result3);
+        
 //  Expected Output :
 //        43
 //        1
